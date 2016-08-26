8734cfede7e37eed89ce1603e69701ff872f0c32
nosyarg <york17a@ncssm.edu>
2016-08-26 12:41:29 -0400

    added global variable for classes per student

--- bubblemodel.py
+++ bubblemodel.py
@@ -1,3 +1,17 @@
+#We will assume that this is constant throughout the model.
+global classesperstudent = 6.022448979591837
+#let us start out with a rotate function
+def rotate(M,oldstud,newstud):
+        #this function should move each of the students from their current year
+        #to the next year and reassign classes appropriately.
+        #If I am feeling motivated, I will add the 5% drop
+        Mt = zip(*M)
+        classperyear = []
+        classfreqbyyear = []
+        for year in Mt:
+                classperyear.append(sum(year))
+                #classfreqbyyear = year/sum(year)
+        
 #we are starting out with 490 students
 totalstudents = 490
 #having the class names could be useful later
@@ -10,9 +24,7 @@ Matrixt = zip(*Matrix)
 yearclasscounts = []
 for i in Matrixt:
         yearclasscounts.append(sum(i))
-#find the total classes taken at the school
-totalclasses = sum(yearclasscounts)
-#find the average classes per student
-classesperstudent = totalclasses/totalstudents
 #find the believed students per year
-studentsperyear = yearclasscounts/classesperstudent
+#studentsperyear = yearclasscounts/classesperstudent
+#lets get these new students in there
+#start out by finding the relative frequencies of classes by year
